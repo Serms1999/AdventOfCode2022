18a99c2660f13adc051f5788b762dc60d51b5224
Sergio Marin Sanchez <serms1999@gmail.com>
2022-12-14 13:59:48 +0100

    WIP: Day 7, folder name could be repeated
    Signed-off-by: Sergio Marin Sanchez <serms1999@gmail.com>

--- Day7/day7.py
+++ Day7/day7.py
@@ -2,7 +2,7 @@ from IO.IO_module import read_input_lines
 import re
 
 
-def parse_file_system(commands: list) -> dict:
+def parse_file_system(commands: list) -> (dict, dict):
     dirs = {}
     dir_files = {}
     parent_dir, current_dir = [], ''
@@ -10,30 +10,43 @@ def parse_file_system(commands: list) -> dict:
     pat_dir = re.compile(pattern=r'dir (.*)')
     pat_file = re.compile(pattern=r'([0-9]+) (.*)')
     for cmd in commands:
-        if mat := re.match(pattern=pat_cd, string=cmd):
-            if current_dir:
-                parent_dir.append(current_dir)
+        if mat := pat_cd.match(cmd):
+            if not current_dir:
                 current_dir = mat.group(1)
-                dirs[parent_dir[-1]].append(current_dir)
-            else:
-                current_dir = mat.group(1)
+                dirs[current_dir] = []
                 dir_files[current_dir] = []
-            dirs[current_dir] = []
+            else:
+                dir_name = mat.group(1)
+                if dir_name == '..':
+                    current_dir = parent_dir.pop()
+                else:
+                    parent_dir.append(current_dir)
+                    current_dir = dir_name
 
-        elif mat := re.match(pattern=pat_dir, string=cmd):
+        elif mat := pat_dir.match(cmd):
             dir_name = mat.group(1)
             dirs[current_dir].append(dir_name)
+            dirs[dir_name] = []
             dir_files[dir_name] = []
-        elif mat := re.match(pattern=pat_file, string=cmd):
-            dir_files[current_dir].append((mat.group(2), mat.group(1)))
+        elif mat := pat_file.match(cmd):
+            dir_files[current_dir].append((mat.group(2), int(mat.group(1))))
 
-    print(f'{dirs=}, {dir_files=}')
+    return dirs, dir_files
+
+
+def get_dir_size(dir_name: str, dirs: dict, dir_files: dict) -> dict:
+    files_size = sum([size for _, size in dir_files[dir_name]])
+    subdir_size = sum([get_dir_size(subdir, dirs, dir_files) for subdir in dirs[dir_name]])
+    return files_size + subdir_size
 
 
 def main():
-    input_lines = read_input_lines(root_file=__file__, file_name='test_input')
-    file_system = parse_file_system(input_lines)
-    print(file_system)
+    input_lines = read_input_lines(root_file=__file__)
+    dirs, dir_files = parse_file_system(input_lines)
+
+    dir_size = {dir_name: get_dir_size(dir_name, dirs, dir_files) for dir_name in dirs}
+    big_dirs = dict(filter(lambda d: d[1] <= 100_000, dir_size.items()))
+    print(f'{sum(big_dirs.values())=}')
 
 
 if __name__ == '__main__':
