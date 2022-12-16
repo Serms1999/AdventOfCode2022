2257a69a2f643f4d3321984d93a973205f1a7603
Sergio Marin Sanchez <serms1999@gmail.com>
2022-12-16 21:44:14 +0100

    WIP: Day 9 Part 2

--- Day9/day9.py
+++ Day9/day9.py
@@ -2,16 +2,25 @@ from IO.IO_module import read_input_lines
 import numpy as np
 
 
-def check_far_away(head: np.ndarray, tail: np.ndarray) -> np.ndarray:
-    distance = head - tail
+def check_far_away(knot1: np.ndarray, knot2: np.ndarray) -> np.ndarray:
+    distance = knot1 - knot2
     if max(abs(distance)) > 1:
         return distance
     return np.array([])
 
 
+def limit_move(move: np.ndarray) -> np.ndarray:
+    signs = move // abs(move)
+    return np.array([signs[0]*min(move[0], 1), signs[1]*min(move[1], 1)])
+
+
 def main():
-    input_lines = read_input_lines()
-    tail, head = np.array([0, 0]), np.array([0, 0])
+    input_lines = read_input_lines(file_name='test_input')
+    rope = [np.zeros(2, dtype=int), np.zeros(2, dtype=int),
+            np.zeros(2, dtype=int), np.zeros(2, dtype=int),
+            np.zeros(2, dtype=int), np.zeros(2, dtype=int),
+            np.zeros(2, dtype=int), np.zeros(2, dtype=int),
+            np.zeros(2, dtype=int), np.zeros(2, dtype=int)]
     moves = {'L': np.array([0, -1]), 'U': np.array([1, 0]),
              'R': np.array([0, 1]), 'D': np.array([-1, 0])}
 
@@ -19,11 +28,12 @@ def main():
     for line in input_lines:
         direction, amount = line.split()
         amount = int(amount)
-        for num in range(amount):
-            head += moves[direction]
-            if (move := check_far_away(head, tail)).size > 0:
-                tail += move - moves[direction]
-                t_positions.add((tail[0], tail[1]))
+        for _ in range(amount):
+            rope[0] += moves[direction]
+            for knot1, knot2 in zip(rope, rope[1:]):
+                if (move := check_far_away(knot1, knot2)).size > 0:
+                    knot2 += limit_move(move)
+            t_positions.add((rope[-1][0], rope[-1][1]))
 
     print(f'{len(t_positions)=}')
 
