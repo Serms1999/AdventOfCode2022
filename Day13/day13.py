ab6dd45666380403b99faef3c15e3ed53eb84aca
Sergio Marin Sanchez <serms1999@gmail.com>
2022-12-19 22:45:32 +0100

    WIP: Day 13

--- Day13/day13.py
+++ Day13/day13.py
@@ -9,27 +9,41 @@ def as_list(x) -> list:
         return [x]
 
 
-def check_order(list1: list, list2: list):
-    result = True
-    for e1, e2 in zip(list1, list2):
-        if type(e1) == type(e2) and type(e1) == int:
-            result = result and e1 <= e2
-        else:
-            result = result and check_order(as_list(e1), as_list(e2))
+def compare_list(list1, list2) -> bool:
+    while True:
+        try:
+            e1 = list1.pop(0)
+            e2 = list2.pop(0)
+        except IndexError:
+            if len(list1) == 0:
+                return len(list2) >= 0
+            elif len(list2) == 0:
+                return False
 
-    return result and len(list1) <= len(list2)
+        if type(e1) == type(e2) == int:
+            if e2 < e1:
+                return False
+        else:
+            list_e1, list_e2 = as_list(e1), as_list(e2)
+            if not compare_list(list_e1, list_e2):
+                return False
 
 
 def main():
     input_lines = read_input_lines(file_name='test_input')
 
     cmp_list = [[], [], '']
-    for index, line in enumerate(input_lines):
+    value = 0
+    for index, line in enumerate(input_lines + ['']):
         position = index % 3
         if not line:
-            print(f'{index // 3 + 1}, {check_order(cmp_list[0], cmp_list[1])}')
+            if compare_list(cmp_list[0], cmp_list[1]):
+                value += index
+                print(index // 3 + 1)
             continue
         cmp_list[position] = json.loads(line)
+
+    print(f'{value=}')
 
 
 if __name__ == '__main__':
