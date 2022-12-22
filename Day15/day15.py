49f6c6b35122d2854fc8c3414def95713a8049ad
Sergio Marin Sanchez <serms1999@gmail.com>
2022-12-22 23:28:51 +0100

    WIP: Day 15 Part 2

--- Day15/day15.py
+++ Day15/day15.py
@@ -20,8 +20,9 @@ def parse_sensors(area: list) -> list:
 
 
 def main():
-    input_lines = read_input_lines()
+    input_lines = read_input_lines(file_name='test_input')
     sensors = parse_sensors(input_lines)
+    """
     row = 2_000_000
 
     impossible_points = set()
@@ -44,8 +45,42 @@ def main():
                     impossible_points.remove(beacon_point[0])
             except KeyError:
                 pass
-
     print(f'{len(impossible_points)=}')
+    """
+
+    minimum, maximum = 0, 20
+    impossible_points = dict()
+    for sensor in sensors:
+        sensor_point, beacon_point, distance = sensor.values()
+
+        if minimum <= sensor_point[1] <= maximum:
+            signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)
+            signal_range = set(filter(lambda s: minimum <= s <= maximum, signal_range))
+            if sensor_point[1] not in impossible_points:
+                impossible_points[sensor_point[1]] = set(signal_range)
+            else:
+                impossible_points[sensor_point[1]].update(signal_range)
+
+        for x in range(1, distance + 1):
+            if not (minimum <= sensor_point[1] + x <= maximum and minimum <= sensor_point[1] - x <= maximum):
+                continue
+
+            signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)
+            signal_range = set(filter(lambda s: minimum <= s <= maximum, signal_range))
+            if sensor_point[1] not in impossible_points:
+                impossible_points[sensor_point[1]] = set(signal_range)
+            else:
+                impossible_points[sensor_point[1]].update(signal_range)
+
+            try:
+                if sensor_point[1] in impossible_points:
+                    impossible_points[sensor_point[1]].remove(sensor_point[0])
+                if beacon_point[1] in impossible_points:
+                    impossible_points[beacon_point[1]].remove(beacon_point[0])
+            except KeyError:
+                pass
+
+    print(impossible_points)
 
 
 if __name__ == '__main__':
