0fa937ca690deedc63cdb88b3ad0cf63bea793a5
Sergio Marin Sanchez <serms1999@gmail.com>
2022-12-28 11:00:08 +0100

    WIP: Day 15 Part 2

--- Day15/day15.py
+++ Day15/day15.py
@@ -19,104 +19,6 @@ def parse_sensors(area: list) -> list:
     return sensors
 
 
-def check_all_values(impossible_points: dict, y_index: int) -> bool:
-    if y_index not in impossible_points:
-        return True
-
-    return not isinstance(impossible_points[y_index], range)
-
-
-def update_impossible_points(impossible_points: dict, y_index: int, values: range, aux_range: range) -> None:
-    aux_range_set = set(aux_range)
-    if y_index not in impossible_points:
-        impossible_points[y_index] = set(values)
-    else:
-        impossible_points[y_index].update(values)
-
-    if impossible_points[y_index] == aux_range_set:
-        impossible_points[y_index] = aux_range
-
-
-def get_impossible_points(sensors: list, minimum: int, maximum: int) -> dict:
-    impossible_points = dict()
-    aux_range = range(minimum, maximum + 1)
-    for sensor in sensors:
-        sensor_point, _, distance = sensor.values()
-
-        if check_all_values(impossible_points, sensor_point[1]) and minimum <= sensor_point[1] <= maximum:
-            signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)
-            signal_range = set(filter(lambda s: minimum <= s <= maximum, signal_range))
-            update_impossible_points(impossible_points, sensor_point[1], signal_range, aux_range)
-
-        for x in range(1, distance + 1):
-            if check_all_values(impossible_points, sensor_point[1] + x) and minimum <= sensor_point[1] + x <= maximum:
-                signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)[x:-x]
-                signal_range = set(filter(lambda s: minimum <= s <= maximum, signal_range))
-                update_impossible_points(impossible_points, sensor_point[1] + x, signal_range, aux_range)
-
-            if not (check_all_values(impossible_points, sensor_point[1] - x) and minimum <= sensor_point[1] - x <= maximum):
-                continue
-
-            update_impossible_points(impossible_points, sensor_point[1] - x, signal_range, aux_range)
-
-    return impossible_points
-
-
-def remove_value(ranges: list, value: int) -> None:
-    index = -1
-    for idx, r in enumerate(ranges):
-        if value in r:
-            index = idx
-            break
-
-    if index == -1:
-        return
-
-    r = ranges.pop(index)
-    a, b = min(r), max(r)
-    if low_range := range(a, value):
-        ranges.append(low_range)
-    if high_range := range(value + 1, b + 1):
-        ranges.append(high_range)
-
-
-def remove_range(ranges: list, item: range) -> None:
-    try:
-        remove_value(ranges, min(item))
-        remove_value(ranges, max(item))
-    except ValueError:
-        return
-
-    def check_between(r: range) -> bool:
-        return min(item) >= min(r) or max(item) <= max(r)
-
-    ranges[:] = [r for r in ranges if check_between(r)]
-
-
-def filter_range(r: range, minimum: int, maximum: int) -> range:
-    filtered_list = list(filter(lambda s: minimum <= s <= maximum, r))
-    if filtered_list:
-        return range(min(filtered_list), max(filtered_list) + 1)
-    return range(0, 0)
-
-
-def possible_points(sensors: list, all_possible: dict, range_possible: range) -> None:
-    minimum, maximum = min(range_possible), max(range_possible)
-    for sensor in sensors:
-        sensor_point, _, distance = sensor.values()
-
-        signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)
-        if minimum <= sensor_point[1] <= maximum:
-            remove_range(all_possible[sensor_point[1]], filter_range(signal_range, minimum, maximum))
-
-        for x in range(1, distance + 1):
-            if minimum <= sensor_point[1] + x <= maximum:
-                remove_range(all_possible[sensor_point[1] + x], filter_range(signal_range[x:-x], minimum, maximum))
-
-            if minimum <= sensor_point[1] - x <= maximum:
-                remove_range(all_possible[sensor_point[1] - x], filter_range(signal_range[x:-x], minimum, maximum))
-
-
 def print_sensors(impossible_points, all_possible):
     for y in all_possible:
         print(f'{y}\t', end='')
@@ -136,15 +38,29 @@ def print_sensors(impossible_points, all_possible):
 def main():
     input_lines = read_input_lines(file_name='test_input')
     sensors = parse_sensors(input_lines)
+    minimum, maximum = 0, 4_000_000
 
-    minimum, maximum = 0, 20
-    range_possible = range(minimum, maximum + 1)
-    all_possible = {x: [range_possible] for x in range_possible}
-    possible_points(sensors, all_possible, range_possible)
+    for row in range(maximum):
+        impossible_points = set()
+        for sensor in sensors:
+            sensor_point, beacon_point, distance = sensor.values()
+            if (offset := abs(row - sensor_point[1])) <= distance:
+                signal_range = range(sensor_point[0] - distance, sensor_point[0] + distance + 1)
+                if offset == 0:
+                    impossible_points.update(signal_range)
+                else:
+                    if sensor_point[1] + offset == row:
+                        impossible_points.update(signal_range[offset:-offset])
+                    elif sensor_point[1] - offset == row:
+                        impossible_points.update(signal_range[offset:-offset])
 
-    for i, v in all_possible.items():
-        if v:
-            print(f'{i=}, {v=}')
+        aux_list = list(filter(lambda x: minimum <= x <= maximum, impossible_points))
+        if len(aux_list) < maximum - minimum + 1:
+            break
+
+    x_coord = (set(range(minimum, maximum + 1)) - impossible_points).pop()
+    tuning_frequency = 4_000_000 * x_coord + row
+    print(f'{x_coord=}, {row=}, {tuning_frequency=}')
 
 
 if __name__ == '__main__':
