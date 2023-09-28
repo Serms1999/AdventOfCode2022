607c72da8fdb93185fc31ad5b19fc7dc684dff46
Sergio Marin Sanchez <serms1999@gmail.com>
2023-09-28 14:28:34 +0200

    WIP: Day 17 part 1

--- Day17/day17.py
+++ Day17/day17.py
@@ -5,7 +5,7 @@ rock_patterns = ('-', '+', '⅃', '|', '■')
 moves = {'>': (1, 0), '<': (-1, 0), 'down': (0, -1)}
 
 
-def sum_tuples(a: tuple, b: tuple) -> tuple:
+def sum_tuples(a: tuple[int, int], b: tuple[int, int]) -> tuple:
     return tuple(map(sum,zip(a, b)))
 
 
@@ -33,12 +33,13 @@ def rock_height(rock: str) -> list:
     if rock == '+':
         return [(0, 2), (1, 3), (2, 2)]
     if rock == '⅃':
-        return [(1, 3)]
+        return [(2, 3)]
     if rock == '|':
         return [(0, 4)]
     return [(0, 2), (1, 2)]
 
 
+"""
 def check_pos(pos: tuple, checks: list, column_height: dict) -> int:
     #############################################################
     # 0 -> MOVABLE                                              #
@@ -49,7 +50,7 @@ def check_pos(pos: tuple, checks: list, column_height: dict) -> int:
     new_checks = [sum_tuples(pos, check) for check in checks]
     if not all(check[0] in range(7) for check in new_checks):
         return 1
-    elif not all(check != (check[0], column_height[check[0]]) for check in new_checks):
+    elif not all(check != (check[0], column_height[check[0]] - 1) for check in new_checks):
         return 2
 
     return 0
@@ -70,24 +71,66 @@ def move_rock(cur_rock: str, pos: [int, int], move: str, column_height: dict) ->
             column_height[new_height[0]] += new_height[1]
         return 2
     return 1
+"""
+
+
+def check_pos(pos: tuple, rock: str, column_height: dict[int, int]) -> bool:
+    checks = [sum_tuples(pos, check) for check in rock_checks(rock)]
+    # Horizontal align
+    if not all(check[0] in range(7) for check in checks):
+        return False
+
+    if not all(check != (check[0], column_height[check[0]]) for check in checks):
+        return False
+
+    return True
+
+
+def move_rock(cur_rock: str, pos: tuple[int, int], movement: str, column_height: dict[int, int]) -> tuple[int, int]:
+    new_pos = sum_tuples(pos, moves[movement])
+    can_move = check_pos(new_pos, cur_rock, column_height)
+    if can_move:
+        return new_pos
+
+    new_heights = [(pos[0] + h[0], h[1]) for h in rock_height(cur_rock)]
+    for new_height in new_heights:
+        column_height[new_height[0]] += new_height[1]
+    return pos
+
+
+# Function to print the current state with # as the column heights and · as spaces of the game and the current falling rock as @
+def print_current_state(column_height: dict, cur_rock: str, pos: tuple):
+    print()
+    for i in range(max(column_height.values()) + 3, -1, -1):
+        for j in range(7):
+            if column_height[j] == i:
+                print('#', end='')
+            elif pos[0] == j and pos[1] == i:
+                print('@', end='')
+            else:
+                print('·', end='')
+        print()
+    print(cur_rock)
+    print()
 
 
 def main():
     jets = read_input_lines(file_name='test_input')[0]
-    column_height = {i: -1 for i in range(7)}
+    column_height = {i: 0 for i in range(7)}
     jet_index = 0
-    prev_rock = '■'
+    prev_rock = rock_patterns[len(rock_patterns) - 1]  # ■ last rock
 
     for index in range(2022):
         cur_rock = new_rock(prev_rock)
-        pos = [2, max(column_height.values()) + 4]
+        pos = (2, max(column_height.values()) + 3)
         while True:
-            move_rock(cur_rock, pos, jets[jet_index], column_height)
+            pos = move_rock(cur_rock, pos, jets[jet_index], column_height)
             jet_index = (jet_index + 1) % len(jets)
 
-            ret = move_rock(cur_rock, pos, 'down', column_height)
-            if ret == 2:
+            new_pos = move_rock(cur_rock, pos, 'down', column_height)
+            if pos == new_pos:
                 break
+            pos = new_pos
 
         prev_rock = cur_rock
 
